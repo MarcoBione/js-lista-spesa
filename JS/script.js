a0b451fba559b12c8cdbd5202203ed070fcf497e
MarcoBione <marcobione.work@gmail.com>
2023-03-15 14:32:31 +0100

    Add solution in JS

--- JS/script.js
+++ JS/script.js
@@ -7,4 +7,23 @@ let shopList =[
     'camicia',
     'cappello'
 ];
+//controllo lunghezza array
+console.log(shopList.length);
 
+//recupero il luogo di stampa
+const stamp = document.querySelector('ul');
+//controllo contenuto
+console.log(stamp);
+
+//variabili n° giri stampa
+let numberCicles = 0;
+let counter=0;
+
+//ciclo stampa
+while(numberCicles < shopList.length){
+    stamp.innerHTML += `
+    <li>${shopList[counter]}</li>
+    `;
+    numberCicles++;
+    counter++;
+};
